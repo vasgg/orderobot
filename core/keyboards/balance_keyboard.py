81e9ffaadb96f8c78fc18ed42e1bf09e1950aff1
vas g <vas.g@me.com>
2023-11-07 16:50:23 +0400

    add balance keyboard

--- core/keyboards/balance_keyboard.py
+++ core/keyboards/balance_keyboard.py
@@ -2,18 +2,6 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-async def get_back_to_menu_button(state: FSMContext) -> InlineKeyboardMarkup:
-    data = await state.get_data()
-    current_mode = data.get('current_mode', 'customer')
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(text='← Главное меню', callback_data=current_mode),
-            ]
-        ]
-    )
-
-
 async def get_back_to_menu_and_pay_buttons(state: FSMContext) -> InlineKeyboardMarkup:
     data = await state.get_data()
     current_mode = data.get('current_mode', 'customer')
