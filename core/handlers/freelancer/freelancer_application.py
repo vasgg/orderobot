89475a6605c4d132332468199b9e69d58d42a5f3
vas g <vas.g@me.com>
2023-11-07 16:54:02 +0400

    working on applications

--- core/handlers/freelancer/freelancer_application.py
+++ core/handlers/freelancer/freelancer_application.py
@@ -1,10 +1,26 @@
 from aiogram import Bot, F, Router, types
 from aiogram.fsm.context import FSMContext
+import arrow
 
-from core.controllers.order_controllers import get_order
-from core.keyboards.common_keyboards import get_order_actions_keyboard
-from core.keyboards.freelancer.freelancer_keyboard import create_application_buttons
-from core.resources.dictionaries import answer
+from core.controllers.application_controllers import (
+    create_application,
+    del_application,
+    get_application,
+)
+from core.controllers.order_controllers import get_order, get_user
+from core.database.models import User
+from core.keyboards.common_keyboards import (
+    close_button,
+    delete_record_keyboad,
+    get_order_actions_keyboard,
+)
+from core.keyboards.customer.customer_keyboard import application_receive_buttons
+from core.keyboards.freelancer.applications_keyboard import get_application_keyboard
+from core.keyboards.freelancer.freelancer_keyboard import (
+    application_send_buttons,
+    get_application_buttons,
+)
+from core.resources.dict import answer
 from core.resources.states import States
 
 router = Router()
@@ -23,7 +39,8 @@ async def fl_get_order_info_handler(call: types.CallbackQuery, session) -> None:
 
 @router.callback_query(F.data.startswith('take_order:'))
 async def fl_get_order_handler(
-    call: types.CallbackQuery, state: FSMContext,
+    call: types.CallbackQuery,
+    state: FSMContext,
 ) -> None:
     order_id = int(call.data.split(":")[1])
     await state.update_data(application_draft_order_id=order_id)
@@ -34,46 +51,140 @@ async def fl_get_order_handler(
 
 
 @router.message(States.fl_appl_charge)
-async def fl_appl_charge_handler(message: types.Message, state: FSMContext, bot: Bot) -> None:
+async def fl_appl_charge_handler(
+    message: types.Message, state: FSMContext, bot: Bot
+) -> None:
     try:
         charge = int(message.text)
         data = await state.get_data()
         await state.update_data(application_draft_charge=charge)
         await message.delete()
-        await bot.delete_message(chat_id=message.from_user.id, message_id=data['application_draft_message'])
+        await bot.delete_message(
+            chat_id=message.from_user.id, message_id=data['application_draft_message']
+        )
         msg = await message.answer(text=answer['fl_appl_days_reply'])
         await state.update_data(application_draft_message_charge=msg.message_id)
         await state.set_state(States.fl_appl_days)
     except ValueError:
-        await message.delete()
         await message.answer(text=answer['incorrect_charge_reply'].format(message.text))
 
 
 @router.message(States.fl_appl_days)
-async def fl_appl_days_handler(message: types.Message, state: FSMContext, bot: Bot) -> None:
+async def fl_appl_days_handler(
+    message: types.Message, state: FSMContext, bot: Bot
+) -> None:
     try:
         days = int(message.text)
         data = await state.get_data()
         await state.update_data(application_draft_days=days)
         await message.delete()
-        await bot.delete_message(chat_id=message.from_user.id, message_id=data['application_draft_message_charge'])
+        await bot.delete_message(
+            chat_id=message.from_user.id,
+            message_id=data['application_draft_message_charge'],
+        )
         msg = await message.answer(text=answer['fl_appl_message_reply'])
         await state.update_data(application_draft_message_days=msg.message_id)
         await state.set_state(States.fl_appl_message)
     except ValueError:
-        await message.delete()
         await message.answer(text=answer['incorrect_days_reply'].format(message.text))
 
 
 @router.message(States.fl_appl_message)
-async def fl_appl_message_handler(message: types.Message, state: FSMContext, bot: Bot) -> None:
+async def fl_appl_message_handler(
+    message: types.Message, state: FSMContext, bot: Bot
+) -> None:
     message_text = message.text
     await state.update_data(application_draft_message=message_text)
     data = await state.get_data()
     await message.delete()
-    await bot.delete_message(chat_id=message.from_user.id, message_id=data['application_draft_message_days'])
-    await message.answer(text=answer['fl_appl_final_reply'].format(data['application_draft_charge'],
-                                                                   data['application_draft_days'],
-                                                                   message_text),
-                         reply_markup=create_application_buttons)
+    await bot.delete_message(
+        chat_id=message.from_user.id, message_id=data['application_draft_message_days']
+    )
+    await message.answer(
+        text=answer['fl_appl_final_reply'].format(
+            data['application_draft_charge'],
+            data['application_draft_days'],
+            message_text,
+        ),
+        reply_markup=get_application_buttons(data['application_draft_order_id']),
+    )
 
+
+@router.callback_query(F.data.startswith('fl_send_application:'))
+async def fl_send_application_handler(
+    call: types.CallbackQuery, state: FSMContext, user: User, bot: Bot, session
+) -> None:
+    order_id = int(call.data.split(":")[1])
+    order = await get_order(order_id, session=session)
+    data = await state.get_data()
+    await create_application(
+        order_id=order_id,
+        customer_id=order.customer_id,
+        freelancer_id=user.id,
+        fee=data['application_draft_charge'],
+        completion_days=data['application_draft_days'],
+        message=data['application_draft_message'],
+        session=session,
+    )
+    await call.message.edit_text(
+        text=answer["fl_take_order_reply"].format(order.name),
+        # reply_markup=close_button
+        reply_markup=application_send_buttons,
+    )
+    customer = await get_user(user_id=order.customer_id, session=session)
+    await bot.send_message(
+        chat_id=customer.telegram_id,
+        text=answer["fl_take_order_from_customer"].format(order.name),
+        reply_markup=application_receive_buttons,
+    )
+    await call.answer()
+
+
+@router.callback_query(F.data.startswith('fl_del_application:'))
+async def fl_delete_application_dialog(
+    call: types.CallbackQuery,
+) -> None:
+    application_id = int(call.data.split(":")[1])
+    await call.message.answer(
+        text=answer["delete_application_reply"],
+        reply_markup=delete_record_keyboad(
+            mode='application', record_id=application_id
+        ),
+    )
+    await call.answer()
+
+
+@router.callback_query(F.data.startswith('delete_application:'))
+async def customer_delete_application_handler(
+    call: types.CallbackQuery, session
+) -> None:
+    application_id = int(call.data.split(':')[1])
+    await del_application(application_id, session=session)
+    await call.message.edit_text(
+        text=answer["deleted_application_reply"], reply_markup=close_button
+    )
+    await call.answer()
+
+
+@router.callback_query(F.data.startswith('customer_get_appl_info:'))
+async def customer_get_application_info_handler(
+    call: types.CallbackQuery, session
+) -> None:
+    application_id = int(call.data.split(':')[1])
+    application = await get_application(application_id, session=session)
+    created_at = arrow.get(application.created_at)
+    order = await get_order(application.order_id, session=session)
+    freelancer = await get_user(user_id=application.freelancer_id, session=session)
+    await call.message.edit_text(
+        text=answer["application_reply"].format(
+            order.name,
+            application_id,
+            freelancer.fullname,
+            created_at.humanize(locale='ru'),
+            application.fee,
+            application.completion_days,
+            application.message
+        ),
+        reply_markup=get_application_keyboard(application_id),
+    )
+    await call.answer()
--- core/handlers/freelancer/freelancer_menu.py
+++ core/handlers/freelancer/freelancer_menu.py
@@ -3,6 +3,7 @@ from aiogram.fsm.context import FSMContext
 
 from core.controllers.application_controllers import (
     get_applications,
+    get_applications_list_string,
 )
 from core.controllers.order_controllers import (
     get_order,
@@ -11,17 +12,21 @@ from core.controllers.order_controllers import (
     get_unapplied_orders,
     get_user,
 )
-from core.controllers.user_controllers import get_deals_counter, get_time_since_registration
+from core.controllers.user_controllers import (
+    get_deals_counter,
+    get_time_since_registration,
+)
 from core.database.models import User
 from core.keyboards.common_keyboards import (
     account_buttons,
     close_button,
     get_orders_keyboard,
 )
+from core.keyboards.freelancer.applications_keyboard import get_applications_keyboard
 from core.keyboards.freelancer.freelancer_keyboard import (
     get_freelancer_keyboard,
 )
-from core.resources.dictionaries import answer
+from core.resources.dict import answer
 from core.resources.states import States
 
 router = Router()
@@ -59,26 +64,6 @@ async def fl_find_order_handler(
     await state.update_data(orders_list_message_id=orders_list.message_id)
     await call.answer()
 
-    # order = get_order(order_id, session=session)
-    # create_application(
-    #     order_id=order_id,
-    #     customer_id=order.customer_id,
-    #     freelancer_id=user.id,
-    #     session=session,
-    # )
-    # await call.message.edit_text(
-    #     text=answer["fl_take_order_reply"].format(order.name),
-    #     reply_markup=close_button
-    #     # reply_markup=get_order_actions_keyboard(order.id, mode="freelancer"),
-    # )
-    # user = get_user(user_id=order.customer_id, session=session)
-    # await bot.send_message(
-    #     chat_id=user.telegram_id,
-    #     text=answer["fl_take_order_from_customer"].format(order.name),
-    #     reply_markup=application_receive_buttons,
-    # )
-    # await call.answer()
-
 
 @router.callback_query(F.data.startswith('customer_of_order:'))
 async def fl_get_customer_of_order_handler(call: types.CallbackQuery, session) -> None:
@@ -107,8 +92,21 @@ async def fl_get_customer_of_order_handler(call: types.CallbackQuery, session) -
 
 
 @router.callback_query(F.data == "fl_applications")
-async def fl_applications_handler(call: types.CallbackQuery, state: FSMContext) -> None:
-    ...
+async def fl_applications_handler(
+    call: types.CallbackQuery, user: User, session
+) -> None:
+    orders = await get_orders(
+        user_id=user.id, mode="others", status="published", session=session
+    )
+    applications = await get_applications(
+        mode='by_worker', worker_id=user.id, session=session
+    )
+    text = answer["fl_applications_reply"] + get_applications_list_string(applications=applications, mode="freelancer", orders=orders)
+    await call.message.answer_photo(photo=types.FSInputFile(path='core/resources/pictures/applications.jpeg'),
+        caption=text,
+        reply_markup=get_applications_keyboard(orders=orders, applications=applications, mode="freelancer")
+    )
+    await call.answer()
 
 
 # @router.callback_query(F.data == "fl_messages")
@@ -118,11 +116,7 @@ async def fl_applications_handler(call: types.CallbackQuery, state: FSMContext) 
 #     ...
 #
 #
-# @router.callback_query(F.data == "fl_help")
-# async def fl_help_handler(
-#     call: types.CallbackQuery, state: FSMContext, bot: Bot
-# ) -> None:
-#     ...
+
 
 # @router.callback_query(F.data.startswith("fl_get_order_info"))
 # async def read_order_info(call: types.CallbackQuery, state: FSMContext) -> None:
@@ -142,8 +136,8 @@ async def my_account_handler(call: types.CallbackQuery, user: User, session) -> 
     deals = answer["deals_as_freelancer"].format(
         await get_deals_counter(user_id=user.id, mode="freelancer", session=session)
     )
-    await call.message.answer(
-        text=answer["my_account_reply"].format(
+    await call.message.answer_photo(photo=types.FSInputFile(path='core/resources/pictures/account.jpeg'),
+                                    caption=answer["my_account_reply"].format(
             user.fullname, time_since_registration, rating, deals, user.balance
         ),
         reply_markup=account_buttons,
