e169900d85950cb4c125fd018e8f8b3527a0cda0
vas g <vas.g@me.com>
2023-11-21 18:57:23 +0400

    update alembic

--- alembic/env.py
+++ alembic/env.py
@@ -25,7 +25,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-config.set_main_option("sqlalchemy.url", settings.asyncpg_db_url)
+config.set_main_option("sqlalchemy.url", settings.aiosqlite_db_url)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
