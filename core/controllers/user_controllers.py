91351e59df0a919224cd6c8db1bf3e85c935392d
vas g <vas.g@me.com>
2023-11-21 19:04:29 +0400

    work on controller

--- core/controllers/user_controllers.py
+++ core/controllers/user_controllers.py
@@ -5,16 +5,17 @@ import arrow
 from sqlalchemy import Result, select, update
 
 from core.database.models import Order, User
+from core.resources.enums import OrderStatus
 
 
-def create_user_in_db(event, session) -> User:
+async def create_user_in_db(event, session) -> User:
     new_user = User(
         telegram_id=event.from_user.id,
         username=event.from_user.username,
         fullname=event.from_user.full_name,
     )
     session.add(new_user)
-    session.flush()
+    await session.flush()
     return new_user
 
 
@@ -23,7 +24,7 @@ async def get_user_from_db(event, session) -> User:
     result: Result = await session.execute(query)
     user = result.scalar()
     if not user:
-        user = create_user_in_db(event, session)
+        user = await create_user_in_db(event, session)
     return user
 
 
@@ -34,11 +35,11 @@ async def get_deals_counter(
     match mode:
         case 'customer':
             query = select(Order).filter(
-                Order.customer_id == user_id, Order.status == 'done'
+                Order.customer_id == user_id, Order.status == OrderStatus.DONE
             )
         case 'freelancer':
             query = select(Order).filter(
-                Order.worker_id == user_id, Order.status == 'done'
+                Order.worker_id == user_id, Order.status == OrderStatus.DONE
             )
         case _:
             raise ValueError(f"Unknown mode: {mode}")
@@ -63,10 +64,20 @@ async def rename_user(telegram_id: int, new_username: str, session) -> None:
     await session.execute(new_name)
 
 
-async def add_balance_to_user(telegram_id: int, amount: int, session) -> None:
-    new_balance = (
-        update(User)
-        .filter(User.telegram_id == telegram_id)
-        .values(balance=amount)
-    )
+async def change_user_balance(telegram_id: int, current_balance: int, amount: int, mode: Literal['add', 'subtract'], session) -> None:
+    match mode:
+        case 'add':
+            new_balance = (
+                update(User)
+                .filter(User.telegram_id == telegram_id)
+                .values(balance=current_balance + amount)
+            )
+        case 'subtract':
+            new_balance = (
+                update(User)
+                .filter(User.telegram_id == telegram_id)
+                .values(balance=current_balance - amount)
+            )
+        case _:
+            raise ValueError(f"Unknown mode: {mode}")
     await session.execute(new_balance)
